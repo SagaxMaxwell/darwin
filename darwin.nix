19d5a71719f9a5e0d5fa59e9fb810d045f30f48e
SagaxMaxwell <sagax.maxwell@gmail.com>
2026-07-03 18:13:05 +0800

    nix-darwin working system

--- darwin.nix
+++ darwin.nix
@@ -2,4 +2,23 @@
 
 {
   system.stateVersion = 5;
+
+  system.primaryUser = "maxwellsagax";
+
+  nix.enable = false;
+
+  environment.systemPackages = with pkgs; [
+    git
+    vim
+    curl
+    wget
+    fzf
+    eza
+  ];
+
+  system.defaults = {
+    dock.autohide = true;
+    finder.FXPreferredViewStyle = "clmv";
+    NSGlobalDomain.AppleShowAllExtensions = true;
+  };
 }
