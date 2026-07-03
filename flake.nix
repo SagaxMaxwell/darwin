adf52039923893c9f36bcd6bf28f481dd43a9b42
SagaxMaxwell <sagax.maxwell@gmail.com>
2026-07-03 18:02:01 +0800

    init nix-darwin flake

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Maxwell's macOS configuration";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    nix-darwin = {
+      url = "github:LnL7/nix-darwin";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs@{ self, nixpkgs, nix-darwin, home-manager, ... }:
+  let
+    system = "aarch64-darwin";
+  in
+  {
+    darwinConfigurations."Maxwells-MacBook-Pro" = nix-darwin.lib.darwinSystem {
+      inherit system;
+
+      modules = [
+        ./darwin.nix
+      ];
+    };
+  };
+}
